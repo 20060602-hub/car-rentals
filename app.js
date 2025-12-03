50fbf7c274952d6c40d1db1886840267831786aa
20060602-hub <20060602@mydbs.ie>
2025-12-03 14:20:13 +0000

    Add Customer class with getters and toString method

--- app.js
+++ app.js
@@ -289,3 +289,27 @@ public class Rental {
         return days;
     }
 }
+
+public class Customer {
+    private String customerId;
+    private String name;
+
+    public Customer(String customerId, String name) {
+        this.customerId = customerId;
+        this.name = name;
+    }
+
+    // --- Getters ---
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer ID: " + customerId + ", Name: " + name;
+    }
+}
