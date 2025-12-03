ce8c682fe1a3c18e86dfb6a73e915d873822ec3c
20060602-hub <20060602@mydbs.ie>
2025-12-03 14:19:43 +0000

    Add Rental class with getters for car, customer, and days

--- app.js
+++ app.js
@@ -264,3 +264,28 @@ public class CarRentalSystem {
         }
     }
 }
+
+public class Rental {
+    private Car car;
+    private Customer customer;
+    private int days;
+
+    public Rental(Car car, Customer customer, int days) {
+        this.car = car;
+        this.customer = customer;
+        this.days = days;
+    }
+
+    // --- Getters ---
+    public Car getCar() {
+        return car;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public int getDays() {
+        return days;
+    }
+}
